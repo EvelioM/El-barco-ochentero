f5290ceedd91b5d5053ff6b0a7d82ee376deefd6
evelio <evelioamoramarin@gmail.com>
2019-11-17 14:02:12 +0100

    Arreglos

--- src/main/java/com/evelio/elbarcoochentero/activities/FirstFragment.java
+++ src/main/java/com/evelio/elbarcoochentero/activities/FirstFragment.java
@@ -2,20 +2,44 @@ package com.evelio.elbarcoochentero.activities;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ScrollView;
+
+import com.evelio.elbarcoochentero.R;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.evelio.elbarcoochentero.R;
-
 public class FirstFragment extends Fragment {
+
+    private View.OnTouchListener onTouchNew;
+    private ScrollView scroll;
+    private static boolean done = false;
+
+    public FirstFragment(View.OnTouchListener onTouch){
+        this.onTouchNew = onTouch;
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_one, container, false);
+        View view = inflater.inflate(R.layout.fragment_one, container, false);
+        scroll = view.findViewById(R.id.fragment_one);
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                onTouchNew.onTouch(v,event);
+                if(!done){
+                    done = true;
+                    scroll.dispatchTouchEvent(event);
+                }
+                done = false;
+                return true;
+            }
+        });
+        return view;
     }
 }
--- src/main/java/com/evelio/elbarcoochentero/activities/SecondFragment.java
+++ src/main/java/com/evelio/elbarcoochentero/activities/SecondFragment.java
@@ -2,41 +2,49 @@ package com.evelio.elbarcoochentero.activities;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.MediaController;
-import android.widget.Toast;
 import android.widget.VideoView;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.evelio.elbarcoochentero.R;
 
 import java.util.ArrayList;
 
-public class SecondFragment extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SecondFragment extends Fragment {
 
     VideoView videoView;
     ListView listView;
     ArrayList<String> videoList;
     ArrayAdapter adapter;
 
+    private View.OnTouchListener onTouchNew;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_two);
+    public SecondFragment(View.OnTouchListener onTouch){
+        this.onTouchNew = onTouch;
+    }
 
-        videoView = (VideoView) findViewById(R.id.your_video_view);
-        listView = (ListView) findViewById(R.id.listView);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_two, container, false);
+
+        videoView = (VideoView) view.findViewById(R.id.your_video_view);
+        listView = (ListView) view.findViewById(R.id.listView);
+
+        view.setOnTouchListener(onTouchNew);
 
         videoList = new ArrayList<>();
         videoList.add("Video barco");
         videoList.add("Video barco 2");
 
-        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, videoList);
+        adapter = new ArrayAdapter(view.getContext(), android.R.layout.simple_list_item_1, videoList);
         listView.setAdapter(adapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -45,18 +53,20 @@ public class SecondFragment extends AppCompatActivity {
 
                 switch (position){
                     case 0:
-                        videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.video_barco));
+                        videoView.setVideoURI(Uri.parse("android.resource://" + view.getContext().getPackageName() + "/" + R.raw.video_barco));
                         break;
                     case 1:
-                        videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.video_barco2));
+                        videoView.setVideoURI(Uri.parse("android.resource://" + view.getContext().getPackageName() + "/" + R.raw.video_barco2));
                         break;
                     default:
                         break;
                 }
-                videoView.setMediaController(new MediaController(SecondFragment.this));
+                videoView.setMediaController(new MediaController(view.getContext()));
                 videoView.requestFocus();
                 videoView.start();
             }
         });
+
+        return view;
     }
 }
